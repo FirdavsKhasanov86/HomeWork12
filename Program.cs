53ec6d0b1baac028b26ba27990c9d7d955584ca4
user <user@Williams-iMac.local>
2020-04-15 14:46:38 +0500

    Added connection to DB

--- Program.cs
+++ Program.cs
@@ -8,7 +8,17 @@ namespace HomeWork12
     {
         static void Main(string[] args)
         {
-            
+            string conString = @"Data source=192.168.103.131; Initial catalog=testdb; user id=sa; password=Sa123.";
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
+            if (con.State == ConnectionState.Open)
+            {
+                System.Console.WriteLine("Opened!");
+            }
+             else
+            {
+                System.Console.WriteLine("Ooops, troubles with connection!!!");
+            }
         }
     }
 }
