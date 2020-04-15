dc313317f92d4210eb2ad524296f78ccdb84b1df
user <user@Williams-iMac.local>
2020-04-15 20:39:55 +0500

    Added Colore Text

--- Program.cs
+++ Program.cs
@@ -8,14 +8,17 @@ namespace HomeWork12
     {
         static void Main(string[] args)
         {
-            
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("CHOISE YOUR NUMBER:");
             Console.WriteLine("1.SELECT ALL FROM DB Person");
             Console.WriteLine("2.INSERT INTO DB Person");
             Console.WriteLine("3.SelectBy ID in DB Person");
             Console.WriteLine("4.Update infomation clients from DB Person");
             Console.WriteLine("5.Delete user from DB Person");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Red; 
             string choise = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
             if (choise == "1")
             {
                 selectAll();
@@ -23,6 +26,7 @@ namespace HomeWork12
 
             if (choise == "2")
             {
+                Console.ForegroundColor = ConsoleColor.Blue;
                 Console.Write("Write name of Person:");
                 string Name = Console.ReadLine();
                 Console.Write("Write surename of Person:");
@@ -30,16 +34,20 @@ namespace HomeWork12
                 Console.Write("Write middlename of Person:");
                 string MiddleName = Console.ReadLine();
                 Insert(Name, SureName, MiddleName);
+                Console.ForegroundColor = ConsoleColor.White;
             }
 
             if (choise == "3")
             {
+                Console.ForegroundColor = ConsoleColor.Blue;
                Console.Write("Write id for DB Person:");
                int num = int.Parse(Console.ReadLine());
                SelectBy(num);
+               Console.ForegroundColor = ConsoleColor.White;
             }
 
             if(choise == "4"){
+                Console.ForegroundColor = ConsoleColor.Blue;
                 Console.Write("Write name of Person:");
                 string Name = Console.ReadLine();
                 Console.Write("Write surename of Person:");
@@ -47,12 +55,15 @@ namespace HomeWork12
                 Console.Write("Write middlename of Person:");
                 string MiddleName = Console.ReadLine();
               Update(Name, SureName, MiddleName);
+              Console.ForegroundColor = ConsoleColor.White;
             }
 
             if(choise == "5"){
+                Console.ForegroundColor = ConsoleColor.Blue;
                Console.Write("Write id for DELETE client from DB Person:");
                int num = int.Parse(Console.ReadLine());
                 Delete(num);
+                Console.ForegroundColor = ConsoleColor.White;
             }
 
 
@@ -60,6 +71,7 @@ namespace HomeWork12
             //selectAll
             static void selectAll()
             {
+                Console.ForegroundColor = ConsoleColor.Green;
                 string conString = @"Data source=192.168.103.131; Initial catalog=testdb; user id=sa; password=Sa123.";
                 SqlConnection con = new SqlConnection(conString);
                 con.Open();
@@ -71,11 +83,13 @@ namespace HomeWork12
                     System.Console.WriteLine($"ID:{reader.GetValue(0)}, FirstName:{reader.GetValue(1)}, LastName:{reader.GetValue(2)}, MiddleName:{reader.GetValue(3)},BirthDate:{reader.GetValue(4)}");
                 }
                 reader.Close();
+                Console.ForegroundColor = ConsoleColor.White;
             }
 
             //insert
             static void Insert(string Name, string SureName, string MiddleName)
             {
+                Console.ForegroundColor = ConsoleColor.Green;
                 string conString = @"Data source=192.168.103.131; Initial catalog=testdb; user id=sa; password=Sa123.";
                 SqlConnection con = new SqlConnection(conString);
                 con.Open();
@@ -91,6 +105,7 @@ namespace HomeWork12
                     System.Console.WriteLine("Insert command successfull!!!");
                 }
                 con.Close();
+                Console.ForegroundColor = ConsoleColor.White;
             }
 
 
@@ -99,6 +114,7 @@ namespace HomeWork12
             //selectById
             static void SelectBy(int num)
             {
+                Console.ForegroundColor = ConsoleColor.Green;
                 string conString = @"Data source=192.168.103.131; Initial catalog=testdb; user id=sa; password=Sa123.";
                 SqlConnection con = new SqlConnection(conString);
                 con.Open();
@@ -110,11 +126,13 @@ namespace HomeWork12
                     System.Console.WriteLine($"ID:{reader1.GetValue(0)}, FirstName:{reader1.GetValue(1)}, LastName:{reader1.GetValue(2)}, MiddleName:{reader1.GetValue(3)},BirthDate:{reader1.GetValue(4)}");
                 }
                 reader1.Close();
+                Console.ForegroundColor = ConsoleColor.White;
             }
 
 
             //Update
             static void Update(string Name, string SureName, string MiddleName){
+                Console.ForegroundColor = ConsoleColor.Green;
                 string conString = @"Data source=192.168.103.131; Initial catalog=testdb; user id=sa; password=Sa123.";
                 SqlConnection con = new SqlConnection(conString);
                 con.Open();
@@ -127,11 +145,13 @@ namespace HomeWork12
                   System.Console.WriteLine("Updated!!");
                 }
                 con.Close(); 
+                Console.ForegroundColor = ConsoleColor.White;
             }
             
 
             //Delete
             static void Delete(int num){
+                Console.ForegroundColor = ConsoleColor.Green;
                 string conString = @"Data source=192.168.103.131; Initial catalog=testdb; user id=sa; password=Sa123.";
                 SqlConnection con = new SqlConnection(conString);
                 con.Open();
@@ -144,6 +164,7 @@ namespace HomeWork12
                   System.Console.WriteLine("DELETED!!");
                 }
                 con.Close(); 
+                Console.ForegroundColor = ConsoleColor.White;
             }
            
 
